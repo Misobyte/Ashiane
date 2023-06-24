3bcd69639d90df7f2f4ec8b0d6217357f26e9ec4
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-06-24 11:37:31 +0430

    admin panel customization completed

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
+from .models import User
+from .forms import AdminUserCreationForm, AdminUserChangeForm
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    form = AdminUserChangeForm
+    add_form = AdminUserCreationForm
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("phone_number", "full_name", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("phone_number", "email", "full_name", "is_staff")
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
+
+    def get_form(self, request, obj=None, **kwargs):
+        defaults = {}
+        if obj is None:
+            defaults["form"] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 """
 custom setting for other extensions
