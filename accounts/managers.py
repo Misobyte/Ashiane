b049dc7757533868053c1838c31a8d5ee4d4c32f
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-06-25 22:30:50 +0430

    fix bugs from tests

--- accounts/managers.py
+++ accounts/managers.py
@@ -3,22 +3,22 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, phone_number, password, **extra_fields):
+    def create_user(self, username, phone_number, password, email=None, **extra_fields):
         if not phone_number:
             raise ValueError("The given phone_number must be set")
-        user = self.model(username=username, phone_number=phone_number, **extra_fields)
+        user = self.model(username=username, phone_number=phone_number, email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
     
-    def create_user_with_phone(self, username, phone_number, password, **extra_fields):
+    def create_user_with_phone(self, username, phone_number, password, email=None, **extra_fields):
         self.create_user(username=username, phone_number=phone_number, password=password, is_active=True, phone_number_verified=True, **extra_fields)
     
     def create_user_with_email(self, username, email, password, **extra_fields):
         self.create_user(username=username, email=email, password=password, is_active=True, email_verified=True, **extra_fields)
     
-    def create_superuser(self, username, phone_number, email=None, password=None, **extra_fields):
+    def create_superuser(self, username, phone_number, password=None, email=None, **extra_fields):
         extra_fields.setdefault("is_admin", True)
         extra_fields.setdefault("is_active", True)
 
-        return self.create_user(username, phone_number, password, **extra_fields)
+        return self.create_user(username, phone_number, password, email, **extra_fields)
--- accounts/tests.py
+++ accounts/tests.py
@@ -9,31 +9,33 @@ class CustomUserTestCase(TestCase):
         user = User.objects.create(
             phone_number="09024978823",
             email="ad.kiany.2009@gmail.com",
-            full_name="abdolrahman kiany",
+            username="abdolrahman",
             password="thisisapass"
         )
         self.assertEqual(user.phone_number, "09024978823")
         self.assertEqual(user.phone_number.as_e164, "+989024978823")
         self.assertEqual(user.email, "ad.kiany.2009@gmail.com")
-        self.assertEqual(user.full_name, "abdolrahman kiany")
+        self.assertEqual(user.username, "abdolrahman")
         self.assertFalse(user.email_verfied)
         self.assertFalse(user.is_admin)
         self.assertFalse(user.phone_number_verified)
-        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_active)
     
     def test_superuser_create(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
+            username="abdolrahman",
             phone_number="09024978823",
+            password="thisisapass",
             email="ad.kiany.2009@gmail.com",
-            full_name="abdolrahman kiany",
-            password="thisisapass"
         )
         self.assertEqual(admin_user.phone_number, "09024978823")
         self.assertEqual(admin_user.phone_number.as_e164, "+989024978823")
         self.assertEqual(admin_user.email, "ad.kiany.2009@gmail.com")
-        self.assertEqual(admin_user.full_name, "abdolrahman kiany")
+        self.assertEqual(admin_user.username, "abdolrahman")
         self.assertFalse(admin_user.email_verfied)
         self.assertFalse(admin_user.phone_number_verified)
         self.assertTrue(admin_user.is_admin)
         self.assertTrue(admin_user.is_active)
+
+
