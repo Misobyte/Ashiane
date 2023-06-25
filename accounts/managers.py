eefdda47230a79ccd57a01fe0d88d89cac7f301e
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2023-06-25 15:24:01 +0430

    fix email bug

--- accounts/managers.py
+++ accounts/managers.py
@@ -3,18 +3,19 @@ from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, phone_number, email, password, **extra_fields):
+    def create_user(self, phone_number, password, email=None, **extra_fields):
         if not phone_number:
             raise ValueError("The given phone_number must be set")
-        email = self.normalize_email(email)
-
+        if email:
+            email = self.normalize_email(email)
+        print(extra_fields)
         user = self.model(phone_number=phone_number, email=email, **extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
     
     def create_user_with_phone(self, phone_number, password, **extra_fields):
-        self.create_user(phone_number=phone_number, password=password, email=None, is_active=True, phone_number_verified=True, **extra_fields)
+        self.create_user(phone_number=phone_number, password=password, is_active=True, phone_number_verified=True, **extra_fields)
     
     def create_superuser(self, phone_number, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_admin", True)
